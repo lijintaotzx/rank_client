dbdb1eea79f8df02279ed05df50c44ec80404b0b
JintaoLi <lijintaotzx@gmail.com>
2020-04-29 16:07:57 +0800

    add signal to update ranking list

--- ranking/models.py
+++ ranking/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-
 # Create your models here.
+from django.db.models.signals import post_save
+
 
 class ClientScore(models.Model):
     client_name = models.CharField(verbose_name='客户端名称', max_length=64)
@@ -24,3 +25,21 @@ class RankingList(models.Model):
         ordering = ['score']
         verbose_name = '客户端排名表'
         verbose_name_plural = '客户端排名表'
+
+
+# 信号更新排行榜数据
+def update_rank_list(sender, instance, created, **kwargs):
+    if created:
+        filter_count = RankingList.objects.filter(client_name=instance.client_name).count()
+        if filter_count == 0:
+            RankingList.objects.create(client_name=instance.client_name, score=instance.score)
+        elif filter_count == 1:
+            ins = RankingList.objects.get(client_name=instance.client_name)
+            ins.score = instance.score
+            ins.save()
+        else:
+            # TODO 排行榜有多余数据，打印入日志文件，及时处理
+            pass
+
+
+post_save.connect(update_rank_list, ClientScore)
